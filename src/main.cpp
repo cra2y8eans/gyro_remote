57a4deb67b9aad350381924468324b8cfe8cf899
cra2y8eans <crazybeans@163.com>
2025-07-25 18:15:39 +0800

    main

--- src/main.cpp
+++ src/main.cpp
@@ -9,8 +9,10 @@
 // 创建ESP NOW通讯实例
 esp_now_peer_info_t peerInfo;
 
-uint8_t Receiver[] = { 0x08, 0xa6, 0xf7, 0x17, 0x6d, 0x84 }; // ESP32_薄
-// uint8_t Receiver[] = { 0x2c, 0xbc, 0xbb, 0x00, 0x52, 0xd4 }; // ESP32_厚
+uint8_t receiver[] = { 0x08, 0xa6, 0xf7, 0x17, 0x6d, 0x84 }; // ESP32_薄
+// uint8_t receiver[] = { 0x2c, 0xbc, 0xbb, 0x00, 0x52, 0xd4 }; // ESP32_厚
+
+#define DEBUG
 
 #define SDA_PIN 18
 #define SCL_PIN 23
@@ -44,39 +46,62 @@ void OnDataRecv(const uint8_t* mac, const uint8_t* data, int len) {
   // memcpy(&servoAngle, data, sizeof(servoAngle)); // 将收到的消息进行存储
 }
 
+// 串口输出
+void SerialPrint() {
+  Serial.printf("Roll: %.2f, Gyro Roll: %.2f\n", Gyro_data.angle_roll, Gyro_data.gyro_roll);
+  Serial.printf("Pitch: %.2f, Gyro Pitch: %.2f\n", Gyro_data.angle_pitch, Gyro_data.gyro_pitch);
+  xSemaphoreGive(mpu6050Mutex);
+  vTaskDelay(100);
+}
+
+// 读取数据
 void getGyroData(void* pvParameters) {
   mpu6050.begin();
   mpu6050.calcGyroOffsets(true);
   TickType_t       xLastWakeTime = xTaskGetTickCount();
   const TickType_t xPeriod       = pdMS_TO_TICKS(5); // 频率 200Hz → 周期为 1/200 = 0.005 秒 = 5 毫秒
   while (1) {
-    mpu6050.update();
-    Gyro_data.angle_roll  = mpu6050.getAngleY();
-    Gyro_data.gyro_roll   = mpu6050.getGyroY();
-    Gyro_data.angle_pitch = mpu6050.getAngleX();
-    Gyro_data.gyro_pitch  = mpu6050.getGyroX();
-    vTaskDelayUntil(&xLastWakeTime, xPeriod);
+    if (xSemaphoreTake(mpu6050Mutex, portMAX_DELAY)) {
+      // 获取陀螺仪数据
+      mpu6050.update();
+      Gyro_data.angle_roll  = mpu6050.getAngleY();
+      Gyro_data.gyro_roll   = mpu6050.getGyroY();
+      Gyro_data.angle_pitch = mpu6050.getAngleX();
+      Gyro_data.gyro_pitch  = mpu6050.getGyroX();
+      xSemaphoreGive(mpu6050Mutex);
+      vTaskDelayUntil(&xLastWakeTime, xPeriod);
+    } else {
+      Serial.println("Failed to take mutex");
+    }
   }
 }
 
+// 发送数据
 void sendData(void* pvParameters) {
   WiFi.mode(WIFI_STA);                  // 设置wifi为STA模式
   esp_now_init();                       // 初始化ESP NOW
   esp_now_register_send_cb(OnDataSent); // 注册发送成功的回调函数
   esp_now_register_recv_cb(OnDataRecv); // 注册接受数据后的回调函数
   // 注册通信频道
-  memcpy(peerInfo.peer_addr, Receiver, 6); // 设置配对设备的MAC地址并储存，参数为拷贝地址、拷贝对象、数据长度
+  memcpy(peerInfo.peer_addr, receiver, 6); // 设置配对设备的MAC地址并储存，参数为拷贝地址、拷贝对象、数据长度
   peerInfo.channel = 1;                    // 设置通信频道
   esp_now_add_peer(&peerInfo);             // 添加通信对象
+  TickType_t       xLastWakeTime = xTaskGetTickCount();
+  const TickType_t xPeriod       = pdMS_TO_TICKS(10); // 频率 100Hz → 周期为 1/100 = 0.01 秒 = 10 毫秒
 
-#ifdef DEBUG
-  if (esp_now_init() != ESP_OK) {
-    Serial.println("ESP-NOW initialization failed");
-  } else {
-    Serial.println("ESP-NOW initialized successfully");
+  while (1) {
+    float angle_pitch, angle_roll, gyro_pitch, gyro_roll;
+    if (xSemaphoreTake(mpu6050Mutex, 100 / portTICK_PERIOD_MS)) {
+      angle_pitch = Gyro_data.angle_pitch;
+      angle_roll  = Gyro_data.angle_roll;
+      gyro_pitch  = Gyro_data.gyro_pitch;
+      gyro_roll   = Gyro_data.gyro_roll;
+      xSemaphoreGive(mpu6050Mutex);
+    }
+    // 发送数据
+    esp_now_send(receiver, (uint8_t*)&servoAngle, sizeof(servoAngle));
+    vTaskDelayUntil(&xLastWakeTime, xPeriod);
   }
-
-#endif
 }
 
 void setup() {
@@ -88,24 +113,6 @@ void setup() {
   mpu6050Mutex = xSemaphoreCreateMutex();
   xTaskCreate(getGyroData, "getGyroData", 2048, NULL, 1, NULL);
   xTaskCreate(sendData, "sendData", 2048, NULL, 1, NULL); // 创建获取陀螺仪数据的任务
-
-#ifdef DEBUG
-  if (mpu6050Mutex == NULL) {
-    Serial.println("Failed to create mutex");
-  } else {
-    Serial.println("Mutex created successfully");
-  }
-  if (getGyroData == NULL) {
-    Serial.println("Failed to create getGyroData task");
-  } else {
-    Serial.println("getGyroData task created successfully");
-  }
-  if (sendData == NULL) {
-    Serial.println("Failed to create sendData task");
-  } else {
-    Serial.println("sendData task created successfully");
-  }
-#endif
 }
 
 void loop() {
